34268ecd6938996ff94675a273da4138596895e5
BrunoTravassos <brunodefrontin@gmail.com>
2021-06-02 11:52:13 -0300

    repositorio do bootcamp

---
+++ src/main/java/com/project/bootcampSantander/exceptions/BusinessExceptions.java
@@ -0,0 +1,8 @@
+package com.project.bootcampSantander.exceptions;
+
+public class BusinessExceptions extends RuntimeException{
+
+    public BusinessExceptions(String message){
+        super(message);
+    }
+}
